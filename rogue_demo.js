81f697428c037217d28945fe24d4fc47251cbeb6
smcourtenay <smcourtenay@gmail.com>
2015-11-20 14:20:41 -0500

    Step 2: Passing Strings to Arrays
    Allegedly this will allow us to pass hand drawn maps to the map
    rendering function.

--- rogue_demo.js
+++ rogue_demo.js
@@ -4,22 +4,39 @@ window.alert("hello");
 Initial test to make sure all the files are in the right place.
 */
 
+var str_map = '##+#########' +  // todo: remove player, creatures, objects
+            '\n#@....######' +
+            '\n#...R.##...#' +
+            '\n#.$...##.p.#' +
+            '\n###+####...#' +
+            '\n###....=..p#' +
+            '\n########...#' +
+            '\n############';
 
-var map = '##+#########' +  // todo: remove player, creatures, objects
-        '\n#@....######' +
-        '\n#...R.##...#' +
-        '\n#.$...##.w.#' +
-        '\n###+####...#' +
-        '\n###....=..w#' +
-        '\n########...#' +
-        '\n############';
+var map = [ [ '#', '#', '+', '#', '#', '#', '#', '#', '#', '#', '#', '#' ], // todo: remove player, creatures, objects
+            [ '#', '@', '.', '.', '.', '.', '#', '#', '#', '#', '#', '#' ],
+            [ '#', '.', '.', '.', 'R', '.', '#', '#', '.', '.', '.', '#' ],
+            [ '#', '.', '$', '.', '.', '.', '#', '#', '.', 'w', '.', '#' ],
+            [ '#', '#', '#', '+', '#', '#', '#', '#', '.', '.', '.', '#' ],
+            [ '#', '#', '#', '.', '.', '.', '.', '=', '.', '.', 'w', '#' ],
+            [ '#', '#', '#', '#', '#', '#', '#', '#', '.', '.', '.', '#' ],
+            [ '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' ],
+            [ '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' ],
+            [ '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' ],
+            [ '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' ],
+            [ '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' ],
+          ];
 
-function MapDraw(map){
-  return '<pre>' + map.toString() + '</pre>';
-}
+
+var renderCell = cell => '<td>' + cell.toString() + '</td>'.
+    renderRow = row => '<tr>' + (row.map(renderCell).join('')) + '</tr>',
+    renderMap  = mapdata => '<table><tbody>' +(mapdata.map(renderRow).join('')) + '</table></tbody>'
+
+var mapStringToGameMap = mapString => mapString.split('\n').map(row => row.split(''));
+
 
 function onStart(){
-  document.body.innerHTML = MapDraw(map);
+  document.body.innerHTML = renderMap(mapStringToGameMap(str_map));
 }
 
 window.onload = onStart;
